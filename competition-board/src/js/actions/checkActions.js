5fc76a431be4264dc12346d5d3ea95cf6f96efe1
Do <doja@tab101179.corpad.net.local>
2018-09-06 08:35:12 +1000

    updated images and goal sound

--- competition-board/src/js/actions/checkActions.js
+++ competition-board/src/js/actions/checkActions.js
@@ -3,13 +3,12 @@ import axios from "axios"
 const config = { 
     crossdomain: true,
     
-    
 }
 
 export function fetchChecks() {
   return function(dispatch) {
     dispatch({type: "FETCH_CHECK"});
-    axios.get("http://localhost:8888/v1/competition", config)
+    axios.get("https://api.nodeping.com/api/1/checks?token=ZZQTRBFZ-UET5-40HY-8M8C-DO7PWOF3A2D4&customerid=201711022024OB8A4", config)
       .then((response) => {
         dispatch({type: "FETCH_CHECK_FULFILLED", payload: response.data})
       })
--- competition-board/src/js/client.js
+++ competition-board/src/js/client.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
-import Layout from "./components/competitionLayout"
+import Layout from "./components/Layout"
 import store from "./store"
 
 const app = document.getElementById('app')
